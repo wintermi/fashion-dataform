55843ad4bdd153f79ed79b75ea4420b4c0506243
Matthew Winter <33818+wintermi@users.noreply.github.com>
2023-11-21 18:09:17 +1100

    feat: add import and purge scripts for Retail Product Catalog

---
+++ scripts/import_retail_product_catalog.zsh
@@ -0,0 +1,48 @@
+#!/usr/bin/env zsh
+
+################################################################################
+# Environment Variables - Retail API Project
+################################################################################
+
+local RETAIL_PROJECT_ID="winter-dataform"
+local RETAIL_PROJECT_NUMBER="374294533986"
+local RETAIL_LOCATION="global"
+local RETAIL_CATALOG="default_catalog"
+local RETAIL_BRANCH="0"
+local RETAIL_IMPORT_GCS_BUCKET="gs://winter-retail/product_catalog"
+local RETAIL_BQ_DATASET="fashion"
+
+local RETAIL_TMP_DIR="${0:A:h}/../tmp"
+RETAIL_TMP_DIR="${RETAIL_TMP_DIR:A}"
+
+
+################################################################################
+# Import Retail Product Catalog - From BigQuery
+#     Reconciliation Mode set to FULL - Insert, Update and Deletes
+################################################################################
+
+# Output the Import Product Catalog JSON Request Object
+cat <<EOF > "$RETAIL_TMP_DIR/import_product_catalog_request.json"
+{
+  "reconciliationMode": "FULL",
+  "skip_default_branch_protection": true,
+  "inputConfig": {
+    "bigQuerySource": {
+      "projectId": "${RETAIL_PROJECT_ID}",
+      "datasetId": "${RETAIL_BQ_DATASET}",
+      "tableId": "retail_product_catalog",
+      "gcsStagingDir": "${RETAIL_IMPORT_GCS_BUCKET}/staging",
+      "dataSchema": "product"
+    }
+  },
+  "errorsConfig": {
+    "gcsPrefix": "${RETAIL_IMPORT_GCS_BUCKET}/errors"
+  }
+}
+EOF
+
+curl -X POST \
+-H "Authorization: Bearer $(gcloud auth application-default print-access-token --project=${RETAIL_PROJECT_ID})" \
+-H "x-goog-user-project: ${RETAIL_PROJECT_ID}" \
+-H "Content-Type: application/json; charset=utf-8" -d @"$RETAIL_TMP_DIR/import_product_catalog_request.json" \
+"https://retail.googleapis.com/v2/projects/${RETAIL_PROJECT_NUMBER}/locations/${RETAIL_LOCATION}/catalogs/${RETAIL_CATALOG}/branches/${RETAIL_BRANCH}/products:import"
---
+++ scripts/purge_retail_product_catalog.zsh
@@ -0,0 +1,35 @@
+#!/usr/bin/env zsh
+
+################################################################################
+# Environment Variables - Retail API Project
+################################################################################
+
+local RETAIL_PROJECT_ID="winter-dataform"
+local RETAIL_PROJECT_NUMBER="374294533986"
+local RETAIL_LOCATION="global"
+local RETAIL_CATALOG="default_catalog"
+local RETAIL_BRANCH="0"
+local RETAIL_IMPORT_GCS_BUCKET="gs://winter-retail/product_catalog"
+local RETAIL_BQ_DATASET="fashion"
+
+local RETAIL_TMP_DIR="${0:A:h}/../tmp"
+RETAIL_TMP_DIR="${RETAIL_TMP_DIR:A}"
+
+
+################################################################################
+# Purge Retail Product Catalog
+################################################################################
+
+# Output the Purge Product Catalog JSON Request Object
+cat <<EOF > "$RETAIL_TMP_DIR/purge_product_catalog_request.json"
+{
+  "filter": "*",
+  "force": true
+}
+EOF
+
+curl -X POST \
+-H "Authorization: Bearer $(gcloud auth application-default print-access-token --project=${RETAIL_PROJECT_ID})" \
+-H "x-goog-user-project: ${RETAIL_PROJECT_ID}" \
+-H "Content-Type: application/json; charset=utf-8" -d @"$RETAIL_TMP_DIR/purge_product_catalog_request.json" \
+"https://retail.googleapis.com/v2/projects/${RETAIL_PROJECT_NUMBER}/locations/${RETAIL_LOCATION}/catalogs/${RETAIL_CATALOG}/branches/${RETAIL_BRANCH}/products:purge"
