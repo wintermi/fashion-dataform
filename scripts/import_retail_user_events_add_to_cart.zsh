c9822e555cfd200bb8ca3a25d425d614c0543d58
Matthew Winter <33818+wintermi@users.noreply.github.com>
2023-11-29 12:22:28 +1100

    feat: add import Retail User Events for Add to Cart script

---
+++ scripts/import_retail_user_events_add_to_cart.zsh
@@ -0,0 +1,44 @@
+#!/usr/bin/env zsh
+
+################################################################################
+# Environment Variables - Retail API Project
+################################################################################
+
+local RETAIL_PROJECT_ID="winter-dataform"
+local RETAIL_PROJECT_NUMBER="374294533986"
+local RETAIL_LOCATION="global"
+local RETAIL_CATALOG="default_catalog"
+local RETAIL_IMPORT_GCS_BUCKET="gs://winter-retail/user_events"
+local RETAIL_BQ_DATASET="fashion"
+
+local RETAIL_TMP_DIR="${0:A:h}/../tmp"
+RETAIL_TMP_DIR="${RETAIL_TMP_DIR:A}"
+
+
+################################################################################
+# Import Retail User Events - Add to Cart - From BigQuery
+################################################################################
+
+# Output the Import User Events - Add to Cart - JSON Request Object
+cat <<EOF > "$RETAIL_TMP_DIR/import_user_events_add_to_cart_request.json"
+{
+  "inputConfig": {
+    "bigQuerySource": {
+      "projectId": "${RETAIL_PROJECT_ID}",
+      "datasetId": "${RETAIL_BQ_DATASET}",
+      "tableId": "retail_user_events_add_to_cart",
+      "gcsStagingDir": "${RETAIL_IMPORT_GCS_BUCKET}/staging",
+      "dataSchema": "user_event"
+    }
+  },
+  "errorsConfig": {
+    "gcsPrefix": "${RETAIL_IMPORT_GCS_BUCKET}/errors"
+  }
+}
+EOF
+
+curl -X POST \
+-H "Authorization: Bearer $(gcloud auth application-default print-access-token --project=${RETAIL_PROJECT_ID})" \
+-H "x-goog-user-project: ${RETAIL_PROJECT_ID}" \
+-H "Content-Type: application/json; charset=utf-8" -d @"$RETAIL_TMP_DIR/import_user_events_add_to_cart_request.json" \
+"https://retail.googleapis.com/v2/projects/${RETAIL_PROJECT_NUMBER}/locations/${RETAIL_LOCATION}/catalogs/${RETAIL_CATALOG}/userEvents:import"
